399a9bd38c7c3bca6a97322ac14ccc020f8b1764
le-nate <99023298+le-nate@users.noreply.github.com>
2024-04-19 12:24:51 +0200

    add: regression dict for smoothed regressions

--- scripts/simulation_regression.py
+++ scripts/simulation_regression.py
@@ -73,32 +73,32 @@ def main() -> None:
     i = sm.add_constant(np.column_stack((x1, x2)))
     model = sm.OLS(y_ai, i)
     results_actual = model.fit()
-    print("\n\n-----Actual model-----\n")
-    print(results_actual.summary())
 
     # * Regress error model
     y_ai_er = error_model(x1_er, x2, error_u, error_v)
     i = sm.add_constant(np.column_stack((x1_er, x2)))
     model = sm.OLS(y_ai_er, i)
     results_error = model.fit()
-    print("\n\n-----Error model-----\n")
-    print(results_error.summary())
 
     # * Calculate differences between actual model and error model coefficients
     for i, j in enumerate(["alpha0", "alpha1", "alpha2"]):
         print(
-            f"\n-----% of stderr for {j}: {(results_actual.params[i] - results_error.params[i])/ results_actual.bse[i]}-----\n"
+            f"""\n-----% of stderr for {j}: {(
+                results_actual.params[i] - results_error.params[i]
+            )/ results_actual.bse[i]}-----\n"""
         )
 
     # * Denoise error model with cumulative subtraction of D1, D2, and D3 crystals
     mother = "sym12"
-    # levels = 6
-    smooth_signals, dwt_levels = dwt.smooth_signal(x1_er, mother)  # , levels=levels)
+    levels = 6
+    smooth_signals, dwt_levels = dwt.smooth_signal(x1_er, mother, levels=levels)
 
-    # TODO * Regress smoothed model
+    # * Regress smoothed model
+    regressions_dict = {}
     crystals = [1, 2, 3]
     for c in crystals:
         print(c)
+        regressions_dict[c] = {}
         x = smooth_signals[c]["signal"]
         y = error_model(x, x2, error_u, error_v)
         i = sm.add_constant(np.column_stack((x, x2)))
@@ -108,44 +108,46 @@ def main() -> None:
         print(f"-----Smoothed model, X1a_er - D_{[i for i in range(1, c+1)]}-----")
         print("\n")
         print(results_smooth.summary())
+        regressions_dict[c]["y"] = y
+        regressions_dict[c]["results"] = results_smooth
 
-    # * Calculate differences between actual model and error model coefficients
-    for i, j in enumerate(["alpha0", "alpha1", "alpha2"]):
-        print(
-            f"\n-----% of stderr for {j}: {(results_actual.params[i] - results_error.params[i])/ results_actual.bse[i]}-----\n"
-        )
+    # * Plot smoothing
+    fig1 = dwt.plot_smoothing(
+        smooth_signals, x=i_values, y=x1_er, name="Error model", figsize=(10, 10)
+    )
+    print(smooth_signals.keys())
+    plt.xlabel("i")
+    plt.ylabel("Error model")
+    fig1.suptitle(f"Wavelet smoothing of Error model (J={dwt_levels})")
+    fig1.tight_layout()
 
-    # # # * Plot smoothing
-    # fig1 = dwt.plot_smoothing(
-    #     smooth_signals, x=i_values, y=x1_er, name="Error model", figsize=(10, 10)
-    # )
-    # print(smooth_signals.keys())
-    # plt.xlabel("i")
-    # plt.ylabel("Error model")
-    # fig1.suptitle(f"Wavelet smoothing of Error model (J={dwt_levels})")
-    # fig1.tight_layout()
+    # * Define subplots
+    fig2, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True)
 
-    # # * Define subplots
-    # fig2, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True)
+    # * Plot regression of actual model
+    plot_fit(results_actual, exog_idx=2, ax=ax1)
+    ax1.plot(x2, y_ai, label="y_ai")
+    ax1.set_title(
+        rf"Unobservable model, $\alpha_{{1}}$: {results_actual.params[1]}, $\sigma^{{2}}$: {results_actual.bse[1]}"
+    )
 
-    # # * Plot regression of actual model
-    # plot_fit(results_actual, exog_idx=2, ax=ax1)
-    # ax1.plot(x2, y_ai, label="y_ai")
-    # ax1.set_title("Unobservable model")
+    # * Plot regression of error model
+    plot_fit(results_error, exog_idx=2, ax=ax2)
+    ax2.plot(x2, y_ai_er, label="y_ai_er", color="orange")
+    ax2.set_title(
+        rf"Error model, $\alpha_{{1}}$: {results_error.params[1]}, $\sigma^{{2}}$: {results_error.bse[1]}"
+    )
 
-    # # * Plot regression of error model
-    # plot_fit(results_error, exog_idx=2, ax=ax2)
-    # ax2.plot(x2, y_ai_er, label="y_ai_er", color="orange")
-    # ax2.set_title("Error model")
+    # TODO Plot regression of smoothed model
+    plot_fit(regressions_dict[3]["results"], exog_idx=2, ax=ax3)
+    ax3.plot(x2, regressions_dict[3]["y"], label=r"y($S_{{3}}$)", color="pink")
+    ax3.set_title(
+        rf"""$S_{{3}}$ model, $\alpha_{{1}}$: {regressions_dict[3]["results"].params[1]}, $\sigma^{{2}}$: {regressions_dict[3]["results"].bse[1]}"""
+    )
 
-    # # TODO Plot regression of smoothed model
-    # plot_fit(results_error, exog_idx=2, ax=ax2)
-    # ax2.plot(x2, y_ai_er, label="y_ai_er", color="orange")
-    # ax2.set_title("Error model")
-
-    # plt.legend(loc="best")
-    # plt.tight_layout()
-    # plt.show()
+    plt.legend(loc="best")
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == "__main__":
