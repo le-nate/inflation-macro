5c2e68fa2b547bcb430e6fd4c172c4d87c3f355a
le-nate <99023298+le-nate@users.noreply.github.com>
2024-04-19 17:10:08 +0200

    fix: trim_signal refactor: plot smoothing

--- scripts/dwt.py
+++ scripts/dwt.py
@@ -2,8 +2,10 @@
 Smoothing of signals via wavelet reconstruction
 """
 
-import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.figure
+import numpy as np
+import numpy.typing as npt
 import pywt
 
 from analysis import retrieve_data as rd
@@ -13,100 +15,119 @@ def trim_signal(original_signal: list, reconstructed: list) -> list:
     """Removes first or last observation for odd-numbered datasets"""
     ## Time series with uneven result in mismatched lengths with the reconstructed
     ## signal, so we remove a value from the approximated signal
-    if len(original_signal) % 2 != 0:
+    if len(reconstructed) % 2 != 0:
         print(
             f"""Odd number of observations dectected (Length: {len(original_signal)}).
              Trim data? (y/n)"""
         )
         trim = input()
+        if trim == "y":
+            print("Trim beginning or end of time series? (b/e)")
+            trim2 = input()
+            if trim2 == "b":
+                reconstructed = reconstructed[1:]
+                return reconstructed
+            elif trim2 == "e":
+                reconstructed = reconstructed[:-1]
+                return reconstructed
     else:
-        trim = "n"
-
-    if trim == "y":
-        print("Trim beginning or end of time series? (begin/end)")
-        trim = input()
-    if trim == "begin":
-        trimmed_signal = reconstructed[1:]
-    elif trim == "end":
-        trimmed_signal = reconstructed[:-1]
-    else:
-        trimmed_signal = reconstructed
-    return trimmed_signal
+        return reconstructed
 
 
-def run_dwt(signal: list, wavelet: str) -> tuple[int, list]:
+def run_dwt(
+    signal: npt.NDArray, wavelet: str, levels: int = None
+) -> tuple[int, npt.NDArray]:
     """Generate levels and coefficients from discrete wavelet transform with
     given wavelet function"""
     ## Define the wavelet type
     w = pywt.Wavelet(wavelet)
     ## Choose the maximum decomposition level
-    dwt_levels = pywt.dwt_max_level(data_len=len(signal), filter_len=w.dec_len)
-    print(
-        f"Max decomposition level of {dwt_levels} for time series length of {len(signal)}"
-    )
-
+    if levels is None:
+        dwt_levels = pywt.dwt_max_level(data_len=len(signal), filter_len=w.dec_len)
+        print(
+            f"Max decomposition level of {dwt_levels} for time series length of {len(signal)}"
+        )
+    else:
+        dwt_levels = levels
+        print(f"DWT with {dwt_levels} levels as defined in levels={levels}")
     dwt_coeffs = pywt.wavedec(signal, wavelet, level=dwt_levels)
     return dwt_levels, dwt_coeffs
 
 
-def smooth_signal(signal: list, wavelet: str) -> dict:
+def smooth_signal(
+    signal: npt.NDArray, wavelet: str, levels: int = None
+) -> tuple[dict, int]:
     """Generate smoothed signals based off wavelet coefficients for each pre-defined level"""
     ## Initialize dict for reconstructed signals
     signals_dict = {}
 
-    levels, coeffs = run_dwt(signal, wavelet)
+    if levels is None:
+        dwt_levels, coeffs = run_dwt(signal, wavelet)
+    else:
+        dwt_levels, coeffs = run_dwt(signal, wavelet, levels)
 
-    ## Loop through levels and add detail level components
-    for l in range(levels):
+    ## Loop through levels and remove detail level component(s)
+    # ! Note: signal_dict[l] provides the signal with levels <= l removed
+    for l in range(dwt_levels, 0, -1):
+        print(f"s_{l}")
         smooth_coeffs = coeffs.copy()
         signals_dict[l] = {}
         ## Set remaining detail coefficients to zero
-        for coeff in range(1, len(smooth_coeffs) - l):
-            smooth_coeffs[coeff] = np.zeros_like(smooth_coeffs[coeff])
+        for coeff in range(1, l + 1):
+            smooth_coeffs[-1 * coeff] = np.zeros_like(smooth_coeffs[-1 * coeff])
         signals_dict[l]["coeffs"] = smooth_coeffs
         # Reconstruct the signal using only the approximation coefficients
         reconst = pywt.waverec(smooth_coeffs, wavelet)
         signals_dict[l]["signal"] = trim_signal(signal, reconst)
 
-    return signals_dict
+    return signals_dict, dwt_levels
+
+
+def plot_smoothing(
+    smooth_signals: dict, x: npt.NDArray, y: npt.NDArray, name: str, **kwargs
+) -> matplotlib.figure.Figure:
+    """Graph series of smoothed signals with original signal"""
+    fig = plt.figure()
+    # * Loop through levels and add detail level components
+    for i, (level, signal) in enumerate(smooth_signals.items(), 1):
+        smooth_level = len(smooth_signals) - level
+        ## Subplot for each smooth signal
+        plt.subplot(len(smooth_signals), 1, i)
+        plt.plot(x, y, label=name.title())
+        plt.plot(x, signal["signal"])
+        plt.xlabel("Year")
+        plt.grid()
+        plt.title(rf"Approximation: $S_{{j-{smooth_level}}}$")
+        plt.legend()
+    return fig
 
 
 def main() -> None:
     """Run script"""
-    ## Matplotlib Settings
-    small_size = 8
-    medium_size = 10
-    bigger_size = 12
-    plt.rc("font", size=bigger_size)  # controls default text sizes
-    plt.rc("axes", titlesize=bigger_size)  # fontsize of the axes title
-    plt.rc("axes", labelsize=medium_size)  # fontsize of the x and y labels
-    plt.rc("xtick", labelsize=small_size)  # fontsize of the tick labels
-    plt.rc("ytick", labelsize=small_size)  # fontsize of the tick labels
-    plt.rc("legend", fontsize=small_size)  # legend fontsize
-    plt.rc("figure", titlesize=bigger_size)  # fontsize of the figure title
+    # ## Matplotlib Settings
+    # small_size = 8
+    # medium_size = 10
+    # bigger_size = 12
+    # plt.rc("font", size=bigger_size)  # controls default text sizes
+    # plt.rc("axes", titlesize=bigger_size)  # fontsize of the axes title
+    # plt.rc("axes", labelsize=medium_size)  # fontsize of the x and y labels
+    # plt.rc("xtick", labelsize=small_size)  # fontsize of the tick labels
+    # plt.rc("ytick", labelsize=small_size)  # fontsize of the tick labels
+    # plt.rc("legend", fontsize=small_size)  # legend fontsize
+    # plt.rc("figure", titlesize=bigger_size)  # fontsize of the figure title
 
-    raw_data = rd.get_insee_data("000857179")
+    raw_data = rd.get_insee_data("000857180")
     _, t, y = rd.clean_insee_data(raw_data)
 
     ## Define the wavelet type
-    wavelet_type = "db4"
-    smooth_signals = smooth_signal(y, wavelet_type)
+    wavelet_type = "sym12"
+    smooth_signals, _ = smooth_signal(y, wavelet_type)
 
     ## Input name of time series
     print("Enter name of time series (to be included in plot)")
     name = input()
 
-    fig = plt.figure(figsize=(10, 10))
-    ## Loop through levels and add detail level components
-    for i, (level, signal) in enumerate(smooth_signals.items(), 1):
-        ## Subplot for each smooth signal
-        plt.subplot(len(smooth_signals), 1, i)
-        plt.plot(t, y, label=name.title())
-        plt.plot(t, signal["signal"])
-        plt.xlabel("Year")
-        plt.grid()
-        plt.title(rf"Approximation: $S_{{j-{level}}}$")
-        plt.legend()
+    fig = plot_smoothing(smooth_signals, x=t, y=y, name=name, figsize=(10, 10))
 
     plt.xlabel("Year")
     plt.ylabel(f"{name.capitalize()}")
