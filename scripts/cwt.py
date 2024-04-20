4bfc64af2def1ccb21330dcb01bc3cef059c32d3
le-nate <99023298+le-nate@users.noreply.github.com>
2024-04-20 21:26:09 +0200

    fix: coi bleeding over

--- scripts/cwt.py
+++ scripts/cwt.py
@@ -16,8 +16,8 @@ from pycwt.helpers import find
 from analysis import retrieve_data as rd
 
 # * Load dataset
-MEASURE = "CPIAUCSL"
-raw_data = rd.get_fed_data(MEASURE, units="pc1", freqs="m")
+MEASURE = "MICH"
+raw_data = rd.get_fed_data(MEASURE)  # , units="pc1", freqs="m")
 _, t_date, dat = rd.clean_fed_data(raw_data)
 t = np.arange(1, len(dat))
 
@@ -29,7 +29,7 @@ DT = 1 / 12  # In years
 
 # * Define title and labels for plots
 TITLE = MEASURE
-LABEL = "US CPI"
+LABEL = "Expected Inflation (US)"
 UNITS = "%"
 
 # We also create a time array in years.
@@ -41,7 +41,7 @@ t = np.arange(1, N + 1) * DT + t0
 # dat_notrend = dat - np.polyval(p, t - t0)
 std = dat.std()  #! dat_notrend.std()  # Standard deviation
 var = std**2  # Variance
-dat_norm = dat / std  #! dat_notrend / std  # Normalized dataset
+# dat_norm = dat / std  #! dat_notrend / std  # Normalized dataset
 
 # * Define wavelet parameters
 mother = wavelet.Morlet(6)  # Morlet wavelet with :math:`\omega_0=6`.
@@ -52,7 +52,7 @@ alpha, _, _ = wavelet.ar1(dat)  # Lag-1 autocorrelation for red noise
 
 # * Conduct ransformations
 # Wavelet transform
-wave, scales, freqs, coi, fft, fftfreqs = wavelet.cwt(dat_norm, DT, DJ, S0, J, mother)
+wave, scales, freqs, coi, fft, fftfreqs = wavelet.cwt(dat, DT, DJ, S0, J, mother)
 # Inverse wavelet transform
 iwave = wavelet.icwt(wave, scales, DT, DJ, mother) * std
 # Normalized wavelet power spectrum
@@ -97,21 +97,20 @@ scale_avg_signif, tmp = wavelet.significance(
 # * Plot results
 plt.close("all")
 # plt.ioff()
-figprops = {"figsize": (11, 8), "dpi": 72}
-fig = plt.figure(**figprops)
+figprops = {"figsize": (20, 10), "dpi": 72}
+fig, bx = plt.subplots(1, 1, **figprops)
 
-# 1) original series anomaly and the inverse wavelet transform
-ax = plt.axes([0.1, 0.75, 0.65, 0.2])
-ax.plot(t, iwave, "-", linewidth=1, color=[0.5, 0.5, 0.5])
-ax.plot(t, dat, "k", linewidth=1.5)
-ax.set_title("a) {}".format(TITLE))
-ax.set_ylabel(r"{} [{}]".format(LABEL, UNITS))
+# # 1) original series anomaly and the inverse wavelet transform
+# ax = plt.axes([0.1, 0.75, 0.65, 0.2])
+# ax.plot(t, iwave, "-", linewidth=1, color=[0.5, 0.5, 0.5])
+# ax.plot(t, dat, "k", linewidth=1.5)
+# ax.set_title("a) {}".format(TITLE))
+# ax.set_ylabel(r"{} [{}]".format(LABEL, UNITS))
 
 # * Normalized cwt power spectrum, signifance levels, and cone of influence
-# Period scale is logarithmic
-bx = plt.axes([0.1, 0.37, 0.65, 0.28], sharex=ax)
+# ! Period scale is logarithmic
 levels = [0.0625, 0.125, 0.25, 0.5, 1, 2, 4, 8, 16]
-bx.contourf(
+power_spec = bx.contourf(
     t,
     np.log2(period),
     np.log2(power),
@@ -119,6 +118,11 @@ bx.contourf(
     extend="both",
     cmap=plt.cm.jet,
 )
+power_spec.set_clim(
+    np.log2(power).min(),
+    np.log2(power).max(),
+)
+cbar = fig.colorbar(power_spec)
 extent = [t.min(), t.max(), 0, max(period)]
 bx.contour(t, np.log2(period), sig95, [-99, 1], colors="k", linewidths=2, extent=extent)
 bx.fill(
@@ -126,21 +130,25 @@ bx.fill(
     np.concatenate(
         [
             np.log2(coi),
-            [1e-9],
+            [levels[2]],
             np.log2(period[-1:]),
             np.log2(period[-1:]),
-            [1e-9],
+            [levels[2]],
         ]
-    ),
+    ).clip(
+        min=-2.5
+    ),  # ! To keep cone of influence from bleeding off graph
     "k",
     alpha=0.3,
-    hatch="x",
+    hatch="--",
 )
-bx.set_title("b) {} Wavelet Power Spectrum ({})".format(LABEL, mother.name))
 bx.set_ylabel("Period (years)")
 #
 Yticks = 2 ** np.arange(np.ceil(np.log2(period.min())), np.ceil(np.log2(period.max())))
 bx.set_yticks(np.log2(Yticks))
 bx.set_yticklabels(Yticks)
+
+# * Invert y axis
+bx.set_ylim(bx.get_ylim()[::-1])
 
 plt.show()
