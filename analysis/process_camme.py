e6909e7b699516a84a9b629bc376ecd3a60504f2
le-nate <99023298+le-nate@users.noreply.github.com>
2024-05-24 19:46:25 +0200

    add: distinct columns for each year

--- analysis/process_camme.py
+++ analysis/process_camme.py
@@ -1,6 +1,19 @@
 """Script to combine and standardize yearly CAMME responses from separate csv files"""
 
 import logging
+
+# * Logging config
+# logger = logging.getLogger(__name__)
+# LOGNAME = "camme.log"
+# logging.basicConfig(
+#     filename=LOGNAME,
+#     filemode="a",
+#     format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
+#     datefmt="%H:%M:%S",
+#     level=logging.DEBUG,
+# )
+# logger.info("Logs saved to %s", LOGNAME)
+
 import os
 from pathlib import Path
 import time
@@ -19,10 +32,6 @@ from constants.camme import (
     VARS_DICT,
 )
 import data.camme
-
-
-# * Define logging threshold
-logging.basicConfig(level=logging.DEBUG)
 
 # * Get data directory folder
 parent_dir = Path(__file__).parents[1]
@@ -46,7 +55,7 @@ def retrieve_csv_files(
     """Add only standard questionnaire csv file paths to dictionary"""
     for year in dir_dict:
         total_files = sum(len(files) for _, _, files in os.walk(dir_dict[year]["path"]))
-        logging.info("There are %s total files for year %s", total_files, year)
+        logging.debug("There are %s total files for year %s", total_files, year)
         for file in dir_dict[year]["path"].rglob("*"):
             if any(supp in file.name for supp in IGNORE_SUPPLEMENTS):
                 pass
@@ -58,10 +67,35 @@ def retrieve_csv_files(
             else:
                 dir_dict[year]["csv"].append(file)
         files_kept_count = len(dir_dict[year]["csv"])
-        logging.info(
+        logging.debug(
             "%s of %s files maintained for year %s", files_kept_count, total_files, year
         )
     return dir_dict
+
+
+def define_year_columns(year_df: str) -> List[str]:
+    """Extract appropriate columns depending on year and corresponding names"""
+    var_name_year = sorted([int(y) for y in VARS_DICT["inf_exp_qual"]])
+    # Define standardized names for columns to keep
+    final_cols = [c for c in VARS_DICT]
+    if int(year_df) < var_name_year[1]:
+        key_year = str(var_name_year[0])
+        logging.debug(key_year)
+        return [
+            VARS_DICT[c][key_year] for c in final_cols if VARS_DICT[c][key_year] != ""
+        ]
+    elif var_name_year[1] <= int(year_df) < var_name_year[2]:
+        key_year = str(var_name_year[1])
+        logging.debug(key_year)
+        return [
+            VARS_DICT[c][key_year] for c in final_cols if VARS_DICT[c][key_year] != ""
+        ]
+    else:
+        key_year = str(var_name_year[2])
+        logging.debug(key_year)
+        return nested_list_values(
+            [VARS_DICT[c][key_year] for c in final_cols if VARS_DICT[c][key_year] != ""]
+        )
 
 
 def convert_to_year_dataframe(
@@ -70,12 +104,20 @@ def convert_to_year_dataframe(
     """Combine all dataframes into one for all years"""
     df_dict = {}
     for year in dir_dict:
-        logging.debug(year)
+        # Empty DataFrame for complete year's data
         df_complete = []
+        cols = define_year_columns(year)
+        logging.debug("Columns to extract: %s", cols)
         for table in dir_dict[year]["csv"]:
             df = pd.read_csv(table, delimiter=";", encoding="latin-1")
-            df_complete.append(df)
+            df_complete.append(df[cols])
         df_dict[year] = pd.concat(df_complete)
+
+        # Rename to standard columns names
+        df_dict[year].rename(
+            columns={old: new for old, new in zip(df_dict[year].columns, VARS_DICT)},
+            inplace=True,
+        )
         df_dict[year]["year"] = int(year)
         logging.debug("DataFrame shape: %s", df_dict[year].shape)
     return df_dict
@@ -92,15 +134,12 @@ def main() -> None:
     dfs = convert_to_year_dataframe(camme_csv_folders)
     end = time.time()
     dtime = end - start
-    logging.debug("Elapsed time to convert to DataFrames: %s", dtime)
+    logging.info("Elapsed time to convert to DataFrames: %s", dtime)
     ## Elapsed time before change: 11.67s
-    cols = list(nested_dict_values(VARS_DICT))
-    cols = list(nested_list_values(cols))
-    cols.insert(0, "year")
-    logging.debug(cols)
-    print(dfs["2014"][cols].head())
-
-    # TODO add column for year
+    logging.info(dfs["2014"].head())
+    for y, d in dfs.items():
+        logging.info("%s %s", y, d.columns.to_list())
+    print(dfs["2014"].head())
 
 
 if __name__ == "__main__":
