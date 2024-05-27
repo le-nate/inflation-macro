92ecbf9e715c464708d80751f4b8497335f421e5
le-nate <99023298+le-nate@users.noreply.github.com>
2024-05-27 20:51:44 +0200

    add: tests for camme processing

---
+++ tests/test_process_camme.py
@@ -0,0 +1,73 @@
+"""Tests for CAMME data processing"""
+
+import logging
+
+import os
+from pathlib import Path
+import time
+from typing import Dict, List, Tuple, Union
+
+import pandas as pd
+
+from analysis.helpers import nested_dict_values, nested_list_values
+import analysis.process_camme as process_camme
+from constants.camme import (
+    IGNORE_HOUSING,
+    IGNORE_HOUSING_YEARS,
+    IGNORE_SUPPLEMENTS,
+    VARS_DICT,
+)
+
+# * Logging config
+logging.basicConfig(level="DEBUG")
+
+test_dir = process_camme.data_dir
+
+
+def test_filter_files() -> None:
+    """Test that only selects standard CAMME csv files"""
+    logging.info("Retrieving folders")
+    camme_csv_folders = process_camme.retrieve_folders(test_dir)
+    logging.info("Retrieving CSV files")
+    camme_csv_folders = process_camme.retrieve_csv_files(camme_csv_folders)
+    logging.debug(camme_csv_folders)
+    for year, files in camme_csv_folders.items():
+        for file in files["csv"]:
+            assert any(
+                supplemental not in file.name for supplemental in IGNORE_SUPPLEMENTS
+            )
+
+    for year, files in camme_csv_folders.items():
+        for file in files["csv"]:
+            if year in IGNORE_HOUSING_YEARS:
+                assert IGNORE_HOUSING not in file.name
+
+
+def test_columns() -> None:
+    """Test that columns match"""
+    logging.info("Retrieving folders")
+    camme_csv_folders = process_camme.retrieve_folders(test_dir)
+    logging.info("Retrieving CSV files")
+    camme_csv_folders = process_camme.retrieve_csv_files(camme_csv_folders)
+    for year, files in camme_csv_folders.items():
+        year_cols, _ = process_camme.define_year_columns(year)
+        logging.debug(year_cols)
+        for table in files["csv"]:
+            df = pd.read_csv(table, delimiter=";", encoding="latin-1")
+            # Set columns as lowercase since some apparently are read as having
+            # different cases than in their csv file
+            df.columns = df.columns.str.lower()
+            assert all(item in df.columns.to_list() for item in year_cols)
+
+
+def main() -> None:
+    """Run test script"""
+    logging.info("Testing file filtering")
+    test_filter_files()
+    logging.info("Testing column selection")
+    test_columns()
+    logging.info("CAMME test complete")
+
+
+if __name__ == "__main__":
+    main()
