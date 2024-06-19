be778cd5e92f9eaf8f2dbca2e23422689d925e55
le-nate <99023298+le-nate@users.noreply.github.com>
2024-06-19 20:06:59 +0200

    add: cwt test

--- tests/test_cwt.py
+++ tests/test_cwt.py
@@ -14,7 +14,7 @@ from pycwt.helpers import find
 
 from analysis.helpers import define_other_module_log_level
 from analysis import retrieve_data as rd
-from scripts.cwt import set_time_range
+from scripts.cwt import set_time_range, run_cwt
 
 # * Logging settings
 logger = logging.getLogger(__name__)
@@ -24,11 +24,16 @@ logger.addHandler(logging.StreamHandler(sys.stdout))
 
 MEASURE = "MICH"
 DT = 1 / 12
+MOTHER = wavelet.Morlet(f0=6)
 
 logger.info("Test set_time_range")
 raw_data = rd.get_fed_data(MEASURE, units="pc1", freqs="m")
 _, t_date, dat = rd.clean_fed_data(raw_data)
-n, t_test = set_time_range(t_date, DT)
+t_test = set_time_range(t_date, DT)
 assert len(t_test) == len(t_date)
+
+logger.info("Test run_cwt")
+power, period, _, _ = run_cwt(t_test, dat, MOTHER)
+assert len(power) == len(period)
 
 logger.info("CWT test complete!")
