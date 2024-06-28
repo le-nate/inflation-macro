460d4bd49f65bd25c71d99c410652eecf02fb1af
le-nate <99023298+le-nate@users.noreply.github.com>
2024-06-28 14:15:14 +0200

    fix: import and type hinting issues

--- src/cwt.py
+++ src/cwt.py
@@ -16,9 +16,9 @@ import matplotlib.pyplot as plt
 
 import pycwt as wavelet
 
-from helpers import define_other_module_log_level
-import retrieve_data
-from wavelet_helpers import plot_cone_of_influence, plot_signficance_levels
+from .helpers import define_other_module_log_level
+from . import retrieve_data
+from .wavelet_helpers import plot_cone_of_influence, plot_signficance_levels
 
 # * Logging settings
 logger = logging.getLogger(__name__)
