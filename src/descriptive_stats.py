7000f0b1b2ce6d3c29a33389a08649babf8f2aa8
le-nate <99023298+o-nate@users.noreply.github.com>
2024-07-10 14:15:43 +0200

    add: jarque bera test

--- src/descriptive_stats.py
+++ src/descriptive_stats.py
@@ -6,9 +6,12 @@ import sys
 
 from typing import Dict, List, Tuple, Union
 
+import matplotlib.pyplot as plt
 import numpy as np
 import numpy.typing as npt
 import pandas as pd
+from scipy import stats
+import seaborn as sns
 import statsmodels.api as sm
 
 from src.helpers import define_other_module_log_level
@@ -21,11 +24,12 @@ define_other_module_log_level("info")
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler(sys.stdout))
 
-DESCRIPTIVE_STATS = ["count", "mean", "std", "skewness", "kurtosis"]
+DESCRIPTIVE_STATS = ["count", "mean", "std", "skewness", "kurtosis", "Jarque Bera"]
+HYPOTHESIS_THRESHOLD = [0.1, 0.05, 0.001]
 
 
 def parse_results_dict(
-    complete_dict: Dict[str, Dict[str, float]], stats_to_keep: List[str]
+    complete_dict: Dict[str, Dict[str, Union[float, str]]], stats_to_keep: List[str]
 ) -> Dict[str, Dict[str, float]]:
     """Extract only required statistics from dictionary"""
     parsed_dict = {}
@@ -45,7 +49,27 @@ def include_statistic(
     return results_dict
 
 
-# TODO Calculate Jarque-Berra
+# TODO Calculate Jarque-Bera
+def test_jarque_bera(
+    data: pd.DataFrame,
+    date_column: str = "date",
+    add_pvalue_stars: bool = False,
+) -> Dict[str, str]:
+    """Generate dictionary with Jarque-Bera test results for each column"""
+    results_dict = {}
+    cols_to_test = [c for c in data.columns if date_column not in c]
+    for col in cols_to_test:
+        x = data[col].dropna().to_numpy()
+        test_stat, p_value = stats.jarque_bera(x)
+        if add_pvalue_stars:
+            result = str(test_stat)
+            for p_threshold in sorted(HYPOTHESIS_THRESHOLD):
+                result += "*" if p_value <= p_threshold else ""
+        results_dict[col] = result
+    return results_dict
+
+
+# TODO Calculate Shapiro-Wilk for completeness (comparison to J-B)
 
 # TODO Calculate Ljung-Box
 
@@ -54,7 +78,7 @@ def include_statistic(
 
 # TODO Generate summary table
 def create_summary_table(
-    data_dict: Dict[str, Dict[str, float]], export_table: bool = False
+    data_dict: Dict[str, Dict[str, Union[float, str]]], export_table: bool = False
 ) -> Union[Tuple[pd.DataFrame, None], pd.DataFrame]:
     """Create table with descriptive statistics for all datasets with option to export"""
     df = pd.DataFrame(data_dict)
@@ -102,18 +126,38 @@ def main() -> None:
     us_data = us_data.merge(dur_consump, how="left")
     us_data = us_data.merge(save, how="left")
 
+    # * Drop NaNs
+    us_data.dropna(inplace=True)
+
+    print(us_data.head())
+
     results = us_data.describe(percentiles=[0.5]).to_dict()
-    skewness = us_data.skew().to_dict()
-    kurtosis = us_data.kurtosis().to_dict()
+    skewness = us_data.skew(numeric_only=True).to_dict()
+    kurtosis = us_data.kurtosis(numeric_only=True).to_dict()
+    jarque_bera = test_jarque_bera(
+        data=us_data, date_column="date", add_pvalue_stars=True
+    )
+    logger.debug(
+        "skewness: %s \n kurtosis: %s \n Jarque Bera: %s",
+        skewness,
+        kurtosis,
+        jarque_bera,
+    )
     results = include_statistic(
         statistic="skewness", statistic_data=skewness, results_dict=results
     )
     results = include_statistic(
         statistic="kurtosis", statistic_data=kurtosis, results_dict=results
     )
+    results = include_statistic(
+        statistic="Jarque Bera", statistic_data=jarque_bera, results_dict=results
+    )
     results = parse_results_dict(results, DESCRIPTIVE_STATS)
     results_df = create_summary_table(results, export_table=True)
     print(results_df)
+
+    us_data.plot.hist(bins=150, subplots=True, legend=True, layout=(1, 5))
+    plt.show()
 
 
 if __name__ == "__main__":
