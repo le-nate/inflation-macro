14cd99f1e7edc6a66987b666f5874b1330431278
le-nate <99023298+o-nate@users.noreply.github.com>
2024-09-27 20:12:55 +0200

    refactor: stuff I can't remember now

--- scripts/camme_data.py
+++ scripts/camme_data.py
@@ -2,8 +2,10 @@
 
 # %%
 import logging
+from pathlib import Path
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
 
@@ -16,20 +18,24 @@ logger = logging.getLogger(__name__)
 define_other_module_log_level("warning")
 logger.setLevel(logging.DEBUG)
 
+# * Get data directory folder
+parent_dir = Path(__file__).parents[1]
+camme_dir = parent_dir / "data" / "camme"
+
 
 # %%
-df_dict, df_all = preprocess()
-inf_fr = get_fed_data("FRACPIALLMINMEI", units="pc1", freq="m")
-df_inf, _, _ = clean_fed_data(inf_fr)
+df_dict, df_all = preprocess(camme_dir)
+# inf_fr = get_fed_data("FRACPIALLMINMEI", units="pc1", freq="m")
+# df_inf, _, _ = clean_fed_data(inf_fr)
 
 # %%
 print(df_all.info())
 df_all.head()
 
 # %%
-df_inf.rename(columns={"value": "inf_measured"}, inplace=True)
-print(df_inf.info())
-df_inf.head()
+# df_inf.rename(columns={"value": "inf_measured"}, inplace=True)
+# print(df_inf.info())
+# df_inf.head()
 
 # %%
 # * Combine month and year to create date
@@ -47,17 +53,18 @@ df_all["date"] = pd.to_datetime(
 df_all.shape
 
 # %%
-df_combo = df_all.merge(df_inf, how="left")
-print(df_combo.shape)
-df_combo.head()
+# df_combo = df_all.merge(df_inf, how="left")
+# print(df_combo.shape)
+# df_combo.head()
+df_combo = df_all.copy()
 
 # %%
 data = df_combo[df_combo["date"] >= "2004-01-01"][
-    ["date", "inf_exp_val_inc", "inf_per_val_inc", "inf_measured"]
+    ["date", "inf_exp_val_inc", "inf_per_val_inc"]
 ].melt(
     id_vars=["date"],
     var_name="Measure",
-    value_vars=["inf_exp_val_inc", "inf_per_val_inc", "inf_measured"],
+    value_vars=["inf_exp_val_inc", "inf_per_val_inc"],
     value_name="Value",
 )
 logging.debug(data.shape)
@@ -83,11 +90,11 @@ sns.kdeplot(data=data, x="Value", hue="Measure")
 plt.yscale("log")
 plt.xscale("log")
 
-# hist, bin_edges = np.histogram(x, bins=50, density=False)
-# bin_center = (bin_edges[:-1] + bin_edges[1:]) / 2
+hist, bin_edges = np.histogram(x, bins=50, density=False)
+bin_center = (bin_edges[:-1] + bin_edges[1:]) / 2
 
-# plt.figure()
-# plt.hist(x, bins=50, density=False)
-# plt.errorbar(bin_center, hist, yerr=50, fmt=".")
+plt.figure()
+plt.hist(x, bins=50, density=False)
+plt.errorbar(bin_center, hist, yerr=50, fmt=".")
 
 plt.show()
