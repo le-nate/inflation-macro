834dcaf425514f0ba69d6b1fe7260647dad76024
le-nate <99023298+o-nate@users.noreply.github.com>
2024-09-30 12:37:06 +0200

    refactor: pip installable package

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
+"""
+Pip-installable package for src module.
+
+To install, in the root directory, run: `pip install -e .`
+"""
+
 from setuptools import find_packages, setup
 
 setup(
-    name="analysis",
+    name="src",
     packages=find_packages(),
 )
--- tests/test_xwt.py
+++ tests/test_xwt.py
@@ -8,7 +8,7 @@ import numpy as np
 
 from src.utils.logging_helpers import define_other_module_log_level
 from src import retrieve_data
-from src import wavelet_helpers
+from src.utils import wavelet_helpers
 from src import xwt
 
 # * Logging settings
